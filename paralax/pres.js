3ab41b71d790e2b133bcb455cdeb51b055ffc95e
Arecin0L <betrancourt.lonicera@gmail.com>
2021-10-26 08:57:48 +0200

    paralax

--- paralax/pres.js
+++ paralax/pres.js
@@ -1,7 +1,7 @@
 function randopif(){
     let btn = $(".btn");
-    btn.css({top: Math.floor((Math.random() * 240) + 1) + "px" });
-    btn.css({left: Math.floor((Math.random() * 200) + 1) + "px"});
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
 
    
 }
@@ -10,7 +10,7 @@ randopif();
 function randopif2(){
     let btn = $(".btn2");
     btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
-    btn.css({left: Math.floor((Math.random() * 2000) + 1) + "px"});
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
 
     
 }
@@ -20,9 +20,150 @@ randopif2();
 function randopif3(){
     let btn = $(".btn3");
     btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
-    btn.css({left: Math.floor((Math.random() * 500) + 1) + "px"});
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
 
 }
 randopif3();
 
 //4
+function randopif4(){
+    let btn = $(".btn4");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif4();
+
+//5
+function randopif5(){
+    let btn = $(".btn5");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif5();
+
+
+function randopif6(){
+    let btn = $(".btn6");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif6();
+
+
+function randopif7(){
+    let btn = $(".btn7");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif7();
+
+
+function randopif8(){
+    let btn = $(".btn8");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif8();
+
+
+function randopif9(){
+    let btn = $(".btn9");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif9();
+
+
+function randopif10(){
+    let btn = $(".btn10");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif10();
+
+function randopif11(){
+    let btn = $(".btn11");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif11();
+
+function randopif12(){
+    let btn = $(".btn12");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif12();
+
+function randopif13(){
+    let btn = $(".btn13");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif13();
+
+function randopif14(){
+    let btn = $(".btn14");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif14();
+
+function randopif15(){
+    let btn = $(".btn15");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif15();
+
+function randopif16(){
+    let btn = $(".btn16");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif16();
+
+function randopif17(){
+    let btn = $(".btn17");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif17();
+
+function randopif18(){
+    let btn = $(".btn18");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif18();
+
+function randopif19(){
+    let btn = $(".btn19");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif19();
+
+function randopif20(){
+    let btn = $(".btn20");
+    btn.css({top: Math.floor((Math.random() * 740) + 1) + "px" });
+    btn.css({left: Math.floor((Math.random() * 450) + 1) + "px"});
+
+}
+randopif20();
