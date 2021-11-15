230446d85bb75daffc5d59d52564fedf34f830ac
krissrb26 <87168176+krissrb26@users.noreply.github.com>
2021-11-15 16:13:23 +0100

    outillage php

---
+++ testpara/outillage.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="./testp.css">
+    <title>Outillage</title>
+    
+
+<body>
+  <div class="back">
+<img src="./photo projet 2/outillage/Porsche_911_Turbo_(vue_profil_avant).webp"
+  
+    <a href="./index.html">
+        <div class="truc">
+            <img class="logo" src="./Logo pic/logob.gif" alt="" width="100vw">
+            <h1>tru<span style="color: rgb(183, 99, 204)">C</span></h1>
+        </div>
+    </a>
+    <div class="lienhead">
+        <div class="search">
+
+            <input type="text" placeholder="Rechercher" class="barre">
+            <button type="submit"><a href="./loisir.html"><i
+                        class="fa fa-search"></i></a></button>
+        </div>
+        <div class="spal">
+            <a class="si" href="./page-connexion_php/login.html">Se connecter</a>
+            <a class="p" href="">Panier</a>
+            
+            <a class="ap" href="">A propos</a>
+        </div>
+    </div>
+  </header>
+  <main>
+    <div class="kriss">
+        <h1 class="loni">
+          <span class="fredo">O</span>
+          <span class="fredo">u</span>
+          <span class="fredo">t</span>
+          <span class="fredo">i</span>
+          <span class="fredo">l</span>
+          <span class="fredo">l</span>
+          <span class="fredo">a</span>
+          <span class="fredo">g</span>
+          <span class="fredo">e</span>
+        </h1>
+    </div>    
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path d="M5 6l5 5l5-5l2 1l-7 7l-7-7z" fill="#fff"/><rect x="0" y="0" width="20" height="20" fill="rgba(0, 0, 0, 0)" /></svg>
+      </div>
+      </section>
+    <section class="s2"> 
+         <section class="poign">
+             <img src="border"src="../photo projet 2/outillage/poignee_gonflable.jpg" alt="">
+                   
+                <p class="poignex">Marre de vous accrocher aux poignées de porte, avec vos vêtements ou vos cabas, la société truC en collaboration avec la société Intex le trafiquant de piscine, a la solution grâce à cette poignée de porte gonflable le résultat est là, une prise en main aléatoire est son atout premier !! 
+                    <br>
+                    <br>
+                    Vendu avec ses rustines, gonflage 100% COVID
+                </p>
+      </div>
+        </section>
+            <div class="s3">
+            <div class="tourn">
+                    
+            <img class="border" src="../photo projet 2/outillage/tournevide.jpg">
+
+            <p class="tourext">Tu es un piètre mécanicien mais tu veux que tes amis croient que tu es un pro du bricolage? Le gain de place est juste incroyable, terminé la recherche de vis et de tournevis correspondant, voici le deux en un, le Tournevide!!
+               <br>
+               <br>
+               Le Tournevide est IN-CRO-YABLE !! Il existe en version cruciforme et en version plat pour votre plus grand bonheur, il vous sauvera la vie si votre femme désire accrocher un cadre moche, en disant que cet outil est trop évoluer pour pouvoir l'utiliser pour un simple cadre GIFI moche!
+               </p>
+            </div>
+</section> 
+        
+<section class="s4">
+    <div class="compa">
+
+    <img class="border"src="../photo projet 2/outillage/compas_qui_fait_des_carres.jpg">
+<p class="compex">
+Léonard de Vinci lui même, se servait également de cet instrument et seul l'entreprise truC vous le propose en exclusivité mondiale le Compas carré. En effet le Compas carré réglera beaucoup de conflits entre les partisants du carré et du rond sans angle
+                    <br>
+                    <br>
+                    Il aidera aussi tout les architectes en herbes dans leurs démarches de projets, pour faire rentrer un meuble rond dans une pièce carré ou une pièce ronde avec des meubles carrés.
+                    ATTENTION !!! Jusqu'au 31 Février pour un compas acheté un niveau à bulle fixe offert une exclusivité de l'entreprise truC.
+                  </p>
+
+    </div>
+</section>
+<section class="s5">
+<div class="marto">
+ <img class="border"src="../photo projet 2/outillage/marteau-electrique.jpg" alt="">
+         <p class="marex">
+ le monde à une fois dans sa vie taper au marteau pour faire moulte travaux de ménage domestique, mais voilà votre poigné de quarantenaire, vous fait défaut, nous avons la solution  se marteau électrique vous rendra votre "punch" d'entant.
+                    <br>
+                    <br>
+                    Claude Francois (Professionnel de chez ENGIE) lui même s'en servi pour changer une ampoule et depuis se jour il n'a plus jamais eté embêter adieu les problèmes grâce au Marteau Elec
+                 </p>
+      </div>
+            
+               
+            </div>
+
+            <div class="s6">
+                <div class="robi"
+                <div class="robex">
+                   
+                Encore un produit exclusif dans la boutique , adopter par la chaine GULLI  en personne en association avec Leroy Merlin pour inciter les jeunes à boire du jus d'orange car oui cinq kilos de fruits et légumes par jour c'est bon pour ton corps jeune malandrin
+                    <br>
+                    <br>
+                    Livré avec son filtre anti pépin et anticalcaire ,s'adapte à d'autres genre de fruit et légumes et pourquoi innové en buvant une bonne raclette? les possibilités sont infini.
+             </p>
+            </div>
+       
+    </section>
+    
+</body>
+<script src="./test.js"></script>
+</html>
