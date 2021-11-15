aa9f72d16add4935dfb9be5c4f0299fc29222a41
krissrb26 <87168176+krissrb26@users.noreply.github.com>
2021-11-15 13:46:42 +0100

    g fait des truc

--- testpara/testp.php
+++ testpara/testp.php
@@ -34,11 +34,50 @@
       </div>
     </section>
     <section class="s2">
-      <img src="../photo projet 2/vaisselle/fourchaine.jpg" alt="">
-      
+      <div class="fourch">
+        <img class="border"src="../photo projet 2/vaisselle/fourchaine.jpg" alt="">
+        <p class="fourchtext">Pas besoin de mots, cette image parle d'elle même, ce chef d'oeuvre sera idéal pour tout amateur de gastronomie acrobatique.
+                  <br>
+                  <br>
+                  Un seul mot me vient à l'esprit BONNE CHANCE aux amateurs de petits pois ou tout autres fans de riz!!
+                  une exclusivitée de l'entreprise truC.
+              </p>
+      </div>
     </section>
-    <section class="s3">Section Three</section>
-    <section class="s4">Section Four</section>
+    <section class="s3">
+      <div class="tassbiz">
+
+        <img class="border" src="../photo projet 2/vaisselle/tasse_en_bison.jpg" alt="">
+        <p class="biztext"> Notre tasse en véritable poils de bison de Croatie du Nord chatouillera votre palais de bon matin, en effet cette tasse équipée de sa soucoupe et de sa cuillère, vous feront économiser un maximum d'eau car le poil de bison est inlavable, ainsi vous retrouverez les saveurs de vos thés, soupes, ou tout autres aliments consommés il y a deux ans auparavant, ce qui donnera un goût unique à votre boisson.
+                  <br>
+                  <br>
+                  En plus de ce modèle, un autre est aussi disponible sans avoir été nettoyé, donc à vous les goûts subtiles et délicats des cafards, puces et tout autres déjections selon l'endroit où les poils ont étaient prélevés. 
+                  </p>
+      </div>
+    </section>
+
+    <section class="s4">
+      <div class="selpoa">
+
+      <img class="border" src="../photo projet 2/vaisselle/saliere_et_poivriere.jpg"alt="">
+      <p class="poitext">Encore une nouveautée sur notre site, cet ensemble de salière et de poivrière pour retraité qui ont le temps de manger le dimanche midi après Jean-luc Reichmann, en effet cet objet technique évite à toutes les personnes ayant cette maladie qui ravage les neuronnes j'ai nommé Parkinson, de retapisser la salle à manger complète et de blessé les membres de leur famille par projection de sel ou de poivre dans les yeux ! 
+                <br>
+                <br>
+                IMPORTANT !! En présentant votre carte d'affiliation au régime de retraite cet ensemble vous reviendra à -10% avec le code promo "AssuranceObsèque"
+                </p>
+      </div>  
+      </section>
+      <section class="s5">
+        <div class="verpik">
+
+        <img class="border" src="../photo projet 2/vaisselle/verre_piquant.jpg" alt="">
+        <p class="pext">
+        <p>
+                Un incontournable de la communauté Gitane, enfin chez vous grâce à la société truC, en effet ce verre a pied mi-oeuvre d'art, mi-verre à pied en peau de niglot sauvage te plaira mon cousin et trouvera une place dans ta camping entre la statue de la vierge Marie et le fusil à pompe du cousin Pedro.
+            <br>
+            <br>
+            Il sera aussi efficace pour les conflits avec les CRS qui ne veulent pas te laisser rentrer dans la commune pour un terrain en leurs balancant dans les chicots! 
+           </p>
   </div>
 
       
