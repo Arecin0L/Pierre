623b01edf67458462ed811f0b5810e02b34d3b73
Arecin0L <betrancourt.lonicera@gmail.com>
2021-11-15 08:43:42 +0100

    hyg page

---
+++ test_page/test_hyg.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./test_hyg.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+ <!--
+    <div class="para">
+
+        <section class="text">
+            <a href="#scrollf"></a>
+            <div class="kriss">
+                <h1 class="loni">
+                  <span class="fredo">H</span>
+                  <span class="fredo">y</span>
+                  <span class="fredo">g</span>
+                  <span class="fredo">i</span>
+                  <span class="fredo">è</span>
+                  <span class="fredo">n</span>
+                  <span class="fredo">e</span>
+                </h1>
+            </div>
+        </section>
+
+        
+        <section class="text">
+            <a href="#scrolls"></a>
+            <h1>hey</h1>
+        </section>
+
+        <section class="text">
+            <a href="#scrollt"></a>
+            <h1>hi</h1>
+        </section>
+
+        <section class="text">
+            <a href="#scrollfo"></a>
+        </section>
+
+        <section class="text">
+            <a href="#scrollfi"></a>
+        </section>
+         -->
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
--- test_page/test_page.php
+++ test_page/test_page.php
@@ -6,106 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="./test_page.css">
     <title>Hygiène</title>
-    
-    
-    
 </head>
-<body>
-    <header>
-        <a href="./index.html">
-            <div class="truc">
-                <img class="logo" src="./Logo pic/logob.gif" alt="" width="100vw">
-                <h1>tru<span style="color: rgb(183, 99, 204)">C</span></h1>
-            </div>
-        </a>
-        <div class="lienhead">
-            <div class="search">
-                <input type="text" placeholder="Rechercher" class="barre">
-                <button type="submit"><a href="./photo projet 2/fourchaine.jpg"><i
-                            class="fa fa-search"></i></a></button>
-            </div>
-            <div class="spal">
-                <a class="si" href="./page-connexion_php/login.html">Se connecter</a>
-                <a class="p" href="">Panier</a>
-                
-                <a class="ap" href="">A propos</a>
-            </div>
-        </div>
-    </header>
-  <main>
-    <main>
-        <div class="kriss">
-            <h1 class="loni">
-              <span class="fredo">H</span>
-              <span class="fredo">y</span>
-              <span class="fredo">g</span>
-              <span class="fredo">i</span>
-              <span class="fredo">è</span>
-              <span class="fredo">n</span>
-              <span class="fredo">e</span>
-            </h1>
-        </div>   
-    <section>
-      <article>
-          <div class="img1"> 
-              <div class="loupe-widget js-loupe">
-                  <div class="loupe-widget__loupe js-loupe-element"></div>
-                  <img class="loupe-widget__image js-loupe-image" src="./photo projet 2/hygiene/arrache_q.jpg"   alt="image"       data-image-full="https://img.maxisciences.com/alternative/antibiotique/ornithorynque_5fc3c97dec699540aab523c2047ed609df88becb.jpg"/>
-                  
-              </div><p>L'ARRACHE Q en inox véritable arrive en france, il sera pour vous un allié dans de longues et interminables réunions aux toilettes sur des projets merdiques. En effet sa technologie "to tear out" récurera les plus récalcitrantes traces de votre foufouyon 
-                  <br>
-                  <br>
-                   Fournie avec un flacon de mercurochrome
-              </p>
-          </div>
-          <div class="img2">
-              <div class="loupe-widget js-loupe">
-                  <div class="loupe-widget__loupe js-loupe-element"></div>
-                  <img class="loupe-widget__image js-loupe-image" src="./photo projet 2/hygiene/brosse_sur_doigt.jpg" alt="image"          data-image-full="https://img.maxisciences.com/alternative/antibiotique/ornithorynque_5fc3c97dec699540aab523c2047ed609df88becb.jpg"/>
-              </div><p>Vous avez une haleine à coupée des tôles mais pas temps pour vous brossez les dents? La société truC vous propose dans sa besace cette brosse sur doigt, en polyéthyène véritable, accouplé à un morceau de latex pour un plus grand confort doigtale
-                  <br>
-                  <br>
-                  Cette brosse peut avoir d'autres fonctions, comme vous peignez la moustache ou les soucils donc elle conviendra également pour vous mesdames.
-              </p>
-          </div>
-          <div class="img3">
-              <div class="loupe-widget js-loupe">
-                  <div class="loupe-widget__loupe js-loupe-element"></div>
-                  <img class="loupe-widget__image js-loupe-image" src="./photo projet 2/hygiene/double_toilette.jpg" alt="image"            data-image-full="https://img.maxisciences.com/alternative/antibiotique/ornithorynque_5fc3c97dec699540aab523c2047ed609df88becb.jpg"/>
-              </div><p>Le temps du popo solo est révolu, bienvenu dans le 21ème siècle grâce à notre "DOUBLE POOP" en effet cette innovation est unique au monde et uniquement sur le site de truC. Donc terminé les interminables attentes prolongées alors qu' "Obama veut sortir de la Maison Blanche". Le plus de ce modèle emblématique est que cela ne deviendra plus un tabou, et à vous les concours de toutes sorte (taille,poids,aspect et odeur) d'autant plus que vous pourrez vous faire encouragez
-                  <br>
-                  <br>
-                  Sa deuxième fonctionnalité sont pour les célibataires, à vous les parties endiablés de candy crush interminables les pieds dans l'eau à la fraîche. (PRECAUTION D'EMPLOI) bien attendre que votre coloc ai terminé avant de reclaquer violemment la lunette!!!
 
-              </p>
-          </div>
-          <div class="img4">
-              <div class="loupe-widget js-loupe">
-                  <div class="loupe-widget__loupe js-loupe-element"></div>
-                  <img class="loupe-widget__image js-loupe-image" src="./photo projet 2/hygiene/brosse_cote.jpg" alt="image"            data-image-full="https://img.maxisciences.com/alternative/antibiotique/ornithorynque_5fc3c97dec699540aab523c2047ed609df88becb.jpg"/>
-              </div><p>Encore une exclusivité de l'entreprise truC? Vous êtes né avec des dents à rayer le parquet, et des endroits sont devenus inaccessible pour vous et votre brosse à dents conventionnelle voici la Brosse à d'Angle, petite innovation dans le monde de l'Hygiène bucco-dentaire 
-                  <br>
-                  <br>
-                  Promotion en ce moment pour les fêtes, pour une Brosse à D'Angle achetée ,une Brosse à doigt à payer donc deux colis reçus.
-               </p>
-          </div>
-          <div class="img5">
-              <div class="loupe-widget js-loupe">
-                  <div class="loupe-widget__loupe js-loupe-element"></div>
-                  <img class="loupe-widget__image js-loupe-image" src="./photo projet 2/hygiene/tige_bouchon.jpg" alt="image"            data-image-full="https://img.maxisciences.com/alternative/antibiotique/ornithorynque_5fc3c97dec699540aab523c2047ed609df88becb.jpg"/>
-              </div><p> Vous avez un certain âge ou vous avez simplement trop écouté de Thunderdome pendant les réunions de Tuning sur le parking de Leclercq, pendant les rassemblements, cet objet est le votre, l'essayer c'est l'adopter l'entreprise truC vous propose le Tige-Bouchon
-                  <br>
-                  <br>
-                  Comme son nom l'indique il est composé d'un manche de tire-bouchon et d'une tige oriculaire pour les murs de miel sécher récalcitrant, rien de plus simple pour son fonctionnement, vous vissez jusqu'a se qu'un "CRACK" se produise et tirer d'un coup sec après cela deux options s'offre à vous, soit c'est gagner soit appeler le 15
-              </p>
-          </div>
-      </article>
-  </section>
-</main>
-<footer>
-    
-</footer>
-</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="./zoom.js"></script>
 </html>
