4893856a2ac14b35653327b067d1626b94b2df74
krissrb26 <87168176+krissrb26@users.noreply.github.com>
2021-11-15 11:16:22 +0100

    parapentaxe

--- testpara/testp.php
+++ testpara/testp.php
@@ -16,11 +16,27 @@
     </div>
     <section class="s1">
       <div class="home">
-        <span>Slider Scroll Example</span>
+        
+         <div class="kriss">
+          <h1 class="loni">
+            <span class="fredo">V</span>
+            <span class="fredo">a</span>
+            <span class="fredo">i</span>
+            <span class="fredo">s</span>
+            <span class="fredo">s</span>
+            <span class="fredo">e</span>
+            <span class="fredo">l</span>
+            <span class="fredo">l</span>
+            <span class="fredo">e</span>
+          </h1>
+      </div>    
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"><path d="M5 6l5 5l5-5l2 1l-7 7l-7-7z" fill="#fff"/><rect x="0" y="0" width="20" height="20" fill="rgba(0, 0, 0, 0)" /></svg>
       </div>
     </section>
-    <section class="s2">Section Two</section>
+    <section class="s2">
+      <img src="../photo projet 2/vaisselle/fourchaine.jpg" alt="">
+      
+    </section>
     <section class="s3">Section Three</section>
     <section class="s4">Section Four</section>
   </div>
